5685af7a33b40c7133058cdf44efdc3f7d08cd7f
zloadmin <ded@xakep.ru>
2015-12-12 14:31:15 +0500

    rm Topmixtapes.com

--- Topmixtapes.com/js/main.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-$(function()
-{
-	$('.track_list li').click(function(e) {
-        $('.track_list li').removeClass('current');
-		$(this).addClass('current');
-    });
-});
