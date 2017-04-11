ea77139df0d434939e353d547507fa6ba61f786d
Karpovich Vladimir <dev.karpovich@gmail.com>
2017-04-11 21:01:32 +0500

    backup

--- vstupit-sro.ru/html/scripts/exit.js
+++ vstupit-sro.ru/html/scripts/exit.js
@@ -1,34 +1,48 @@
-$(document).ready(function() {
-    $("a.fancy").fancybox();
-});	
+$(document).ready(function () {
+    $("a.fancy").fancybox({
+        tpl: {
+            closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;" id="calc_close_btn"></a>'
+        }
+    });
 
-$(document).ready(function(){
-	window.tick = false;
-	$(document).mousemove(function(e) {
+    $('.form-step-2 .item-list span').click(function() {
+        check = $(this).parent();
+        list = check.find('.items');
+        if(list.is(':hidden')){
+            list.show();
+        } else {
+            list.hide();
+            list.find('.checked').removeClass('checked');
+            list.find('input:checked').removeAttr('checked');
+        }
+    });
 
-			if(window.tick == false)
-			{
+});
 
-				if(e.clientY <= 5 || e.clientX <= 5)
-				{
-					window.tick = true;
-					$('.fancy').trigger('click');
-				}
-			}
-		});
+$(document).ready(function () {
+    window.tick = false;
+    $(document).mousemove(function (e) {
+
+        if (window.tick == false) {
+
+            if (e.clientY <= 5 || e.clientX <= 5) {
+                window.tick = true;
+                $('.fancy').trigger('click');
+            }
+        }
+    });
 
 
+    $('button').click(function (e) {
+        e.preventDefault();
+    });
+    $('.sendform').click(function (e) {
+        e.preventDefault();
 
-	$('button').click(function(e){
-		e.preventDefault();
-	});
-	$('.sendform').click(function(e){
-		e.preventDefault();
-
-		$('#result').html('<img src="images/loading.gif" alt="" />');
-		$.post('/sendmail.php', $('#qw').serialize(), function(data){
-			$('#m_form #result').html(data);
-		});
-	});
+        $('#result').html('<img src="images/loading.gif" alt="" />');
+        $.post('/sendmail.php', $('#qw').serialize(), function (data) {
+            $('#m_form #result').html(data);
+        });
+    });
 
 });
